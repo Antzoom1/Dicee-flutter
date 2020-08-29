343ef8ae938676f2f374df50b89ecba8657c4b8a
Hxbrs <jchanpog@gmail.com>
2020-08-29 16:50:09 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,29 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var LeftDiceNumber = 1;
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                print('Noah is nooby.');
+              } ,
+              child: Image.asset('images/dice$LeftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Noah still nooby');
+              },
+            child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
